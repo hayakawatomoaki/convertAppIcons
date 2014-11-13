cd0565d486ee8922af06034b92cc35adeb6aab3f
hayakawa <hayakawa@nttr.co.jp>
2014-11-13 19:35:52 +0900

    first commit.

---
+++ config.rb
@@ -0,0 +1,48 @@
+# file name and size
+$icon_ios_array = [
+               { fname: "Icon-29.png", size: 29 },
+               { fname: "Icon-29@2x.png", size: 58},
+               { fname: "Icon-29@3x.png", size: 87},
+               { fname: "Icon-40.png", size: 40 },
+               { fname: "Icon-40@2x.png", size: 80},
+               { fname: "Icon-40@3x.png", size: 120},
+               { fname: "Icon-50.png", size: 50 },
+               { fname: "Icon-50@2x.png", size: 100},
+               { fname: "Icon-50@3x.png", size: 150},
+               { fname: "Icon-57.png", size: 57 },
+               { fname: "Icon-57@2x.png", size: 114},
+               { fname: "Icon-57@3x.png", size: 171},
+               { fname: "Icon-60.png" , size: 60},
+               { fname: "Icon-60@2x.png" , size: 120},
+               { fname: "Icon-60@3x.png" , size: 180},
+               { fname: "Icon-72.png" , size: 72},
+               { fname: "Icon-72@2x.png" , size: 144},
+               { fname: "Icon-72@3x.png" , size: 216},
+               { fname: "Icon-76.png" , size: 76},
+               { fname: "Icon-76@2x.png" , size: 152},
+               { fname: "Icon-76@3x.png" , size: 228},
+               { fname: "Icon-Small-50.png" , size: 50},
+               { fname: "Icon-Small-50@2x.png" , size: 100},
+               { fname: "Icon-Small-50@3x.png" , size: 150},
+               { fname: "Icon-Small.png" , size: 29},
+               { fname: "Icon-Small@2x.png" , size: 58},
+               { fname: "Icon-Small@3x.png" , size: 87},
+               { fname: "Icon.png" , size: 57},
+               { fname: "Icon@2x.png" , size: 114},
+               { fname: "Icon@3x.png" , size: 171},
+               { fname: "iTunesArtwork.png" , size: 512},
+               { fname: "iTunesArtwork@2x.png" , size: 1024},
+               { fname: "iTunesArtwork@3x.png" , size: 1536}
+]
+
+
+$icon_android_array = [
+               { fname: "drawable-hdpi/ic_launcher.png", size: 72 },
+               { fname: "drawable-ldpi/ic_launcher.png", size: 36 },
+               { fname: "drawable-mdpi/ic_launcher.png", size: 48 },
+               { fname: "drawable-xhdpi/ic_launcher.png", size: 96 },
+               { fname: "drawable-xxhdpi/ic_launcher.png", size: 144 },
+               { fname: "drawable-xxxhdpi/ic_launcher.png", size: 192 },
+               { fname: "playstore-icon.png", size: 512 }
+]
+
